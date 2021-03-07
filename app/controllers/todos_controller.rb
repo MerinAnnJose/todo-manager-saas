9e0b3a3adeb3330a8ed7e979da54d8ad8a896efe
Merin Ann Jose <merinannjose.3@gmail.com>
2021-03-07 21:09:12 +0530

    sign-in and ensured user login before action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
 class TodosController < ApplicationController
     def index
-        #render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+        current_user
         render "index"
     end
 
