b8fdc321f73165468fddc1874a99d605fa57412a
Merin Ann Jose <merinannjose.3@gmail.com>
2021-03-07 19:31:19 +0530

    added sign-up and sign-in pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get  "/" => "home#index"
   
   resources :todos
+
+  resources :users
+
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
